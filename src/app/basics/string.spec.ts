29df713aa2a4848477eb95c07ae04d4064ba41b6
adrivegas <adrivegas08@gmail.com>
2020-03-01 15:36:28 -0500

    Prueba para evaluar que la respuesta  contenga algo(to Contain)

--- src/app/basics/string.spec.ts
+++ src/app/basics/string.spec.ts
@@ -5,4 +5,9 @@ describe ('Pruebas de string', () => {
         const resp = mensaje('Adriana');
         expect(typeof resp).toBe('string');
     })
+    it('Debe retornar un saludo con el nombre enviado', () => {
+        const nombre = 'Adriana';
+        const resp = mensaje(nombre);
+        expect(resp).toContain(nombre);
+    })
 })
