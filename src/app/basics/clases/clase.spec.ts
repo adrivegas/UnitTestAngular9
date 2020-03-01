128a5e0220d4a29946a8869d48dada9658ef2d15
adrivegas <adrivegas08@gmail.com>
2020-03-01 16:58:18 -0500

    Probando clases

--- src/app/basics/clases/clase.spec.ts
+++ src/app/basics/clases/clase.spec.ts
@@ -14,9 +14,13 @@ describe('Pruebas de clase', () => {
         expect(resp).toBe(80);
     })
     it('Debe retornar 50 de hp, si recibe 50 de daño', () => {
-        const jugador = new Jugador();
         const resp = jugador.recibeDanio(50);
 
         expect(resp).toBe(50);
+    })
+    it('Debe retornar 0 de hp, si recibe 100 de daño o más', () => {
+        const resp = jugador.recibeDanio(100);
+
+        expect(resp).toBe(0);
     })
 })
