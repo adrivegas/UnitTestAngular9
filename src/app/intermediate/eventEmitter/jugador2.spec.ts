0a6ea8869c8232bfa222c4336ded2b8cddf93ed0
adrivegas <adrivegas08@gmail.com>
2020-03-01 21:19:40 -0500

    Probando EventEmitter

---
+++ src/app/intermediate/eventEmitter/jugador2.spec.ts
@@ -0,0 +1,30 @@
+import { from } from 'rxjs';
+import { Jugador2 } from './jugador2';
+
+describe('Jugador 2 Emit', () => {
+
+    let jugador = new Jugador2();
+    
+    beforeEach(() => jugador = new Jugador2());
+
+    // beforeEach(() => {
+    //     jugador = new Jugador2();
+    // })
+   
+    it('Debe emitir un evento cuando recibe daño', () => {
+        let nuevoHP = 0;
+
+        jugador.hpCambia.subscribe(hp => {
+            nuevoHP = hp;
+        });
+        jugador.recibeDanio(1000);
+        expect(nuevoHP).toBe(0);
+    })
+    it('Debe emitir un evento cuando recibe daño y sobrevivir si es menos de 100', () => {
+        let nuevoHP = 0;
+
+        jugador.hpCambia.subscribe(hp => nuevoHP = hp);
+        jugador.recibeDanio(50);
+        expect(nuevoHP).toBe(50);
+    })
+})
